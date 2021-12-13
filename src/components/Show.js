79aa9dca24acc860db09860b1fe6fa6f5fcb5e58
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-13 17:38:15 -0500

    done testing loading component within show component

--- src/components/Show.js
+++ src/components/Show.js
@@ -4,6 +4,8 @@ import Loading from './Loading';
 
 const Show = (props) => {
     const { handleSelect, selectedSeason, show } = props;
+
+    console.log('SHOW:', show)
     
     if (!show)
       return <Loading />
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -5,9 +5,24 @@ import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
 
-test('renders without errors', ()=>{});
+const testShow = {
+    name: 'Stranger Things',
+    summary: 'blah blah summary',
+    seasons: [
+        {id: 1, name: 'season 1', episodes: []},
+        {id: 2, name: 'season 2', episodes: []},
+    ],
+}
+
+test('renders without errors', ()=>{
+    render(<Show show={testShow} selectedSeason = 'none'/>);
+});
+
 
 test('renders Loading component when prop show is null', () => {});
+    render(<Show show= {null} selectedSeason = 'none'/>);
+    const loading = screen.queryByTestId('loading-container');
+    expect(loading).toBeInTheDocument()
 
 
 test('renders same number of options seasons are passed in', ()=>{});
