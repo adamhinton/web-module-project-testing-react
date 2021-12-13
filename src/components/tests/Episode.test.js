40b6252b0ca87653273f8b4b51ccdd6d1240c46e
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-13 17:14:48 -0500

    done with episode component testing

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,52 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const testEpisode = {
+    id:1,
+    name: '',
+    image: 'http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg',
+    season:1,
+    number:1,
+    summary: 'Heebie Jeebie 1 2 3',
+    runtime: 1,
+}
 
-test("renders without error", () => {});
+// const testEpisodeWithSummary = {
+//     id:1,
+//     name: '',
+//     image: 'http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg',
+//     season:1,
+//     number:1,
+//     summary: 'Heebie Jeebie 1 2 3',
+//     runtime: 1,
+// }
 
-test("renders the summary test passed as prop", ()=>{});
+const testEpisodeWithoutImage={
+        id:1,
+        name: '',
+        image: null,
+        season:1,
+        number:1,
+        summary: 'Heebie Jeebie 1 2 3',
+        runtime: 1,
+}
 
-test("renders default image when image is not defined", ()=>{});
+test("renders without error", () => {
+    render(<Episode episode = {testEpisode}/>)
+});
+
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode ={testEpisode}/>);
+
+    const summaryExists = screen.queryByText(/Heebie Jeebie 1 2 3/i);
+    
+    expect(summaryExists).toBeTruthy();
+    expect(summaryExists).toBeInTheDocument();
+    expect(summaryExists).toHaveTextContent(/Heebie Jeebie 1 2 3/i)
+});
+
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode = {testEpisodeWithoutImage}/>)
+
+    const testImg = screen.queryAllByAltText('./stranger_things.png')
+});
